0f75e3abd009d23ffa2503296dd2489b07bead40
samirvithlani <samir.vithlani83955@gmail.com>
2024-07-22 17:56:30 +0530

    operator

---
+++ operators/comparion/comparision.js
@@ -0,0 +1,14 @@
+// ==, ===
+
+var age = "10"
+var age2 = 10
+
+console.log(age == age2)
+console.log(age === age2)
+
+if(age == age2){
+    console.log("Equal")
+}
+else{
+    console.log("Not Equal")
+}
