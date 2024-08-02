558d5e80a5b28670c800d1404ee5db39554cfc24
samirvithlani <samir.vithlani83955@gmail.com>
2024-08-02 17:24:31 +0530

    callback

---
+++ functions/callback4.js
@@ -0,0 +1,33 @@
+const pdfHandler = (fileName) =>{
+
+    return fileName + ' is a pdf file';
+}
+const jpgHandler = (fileName) =>{
+
+    return fileName + ' is a jpg file';
+}
+const txtHandler = (fileName) =>{
+
+    return fileName + ' is a text file'; 
+}
+
+
+const uploadFile =(fileName,callback)=>{
+
+
+    //console.log(`${fileName} is uploaded`);
+    //pdfHandler(fileName);
+    //var x = callback(fileName);
+    //console.log(x);
+    //return x;
+
+    return callback(fileName);
+
+}
+
+var file = 'sample.pdf';
+var x;
+if(file.endsWith('.pdf')){
+    x = uploadFile(file,pdfHandler);
+}
+console.log(x);
