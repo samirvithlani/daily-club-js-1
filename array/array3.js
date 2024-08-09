0ef70b7d54cc61fa06445a78e120dd9eeaaadaa0
samirvithlani <samir.vithlani83955@gmail.com>
2024-08-09 18:14:45 +0530

    some,every

---
+++ array/array3.js
@@ -0,0 +1,23 @@
+var lang = ["hindi","english","marathi","tamil","telugu","gujarati"];
+
+//loop
+//lang[i]
+var x = lang.some((l)=>{
+
+    //return predicate.. true or false
+    //return  l.startsWith("x")
+    return l.length > 10
+})
+
+console.log(x) //true
+
+var x = lang.every((l)=>{
+    return l.length > 5
+})
+
+console.log(x) //true
+
+
+var x = lang.some((l)=>l.includes("a"))
+console.log(x) //true
+
