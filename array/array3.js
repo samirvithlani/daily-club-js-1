1453b8f9c7269625f6ec5bf53d4166c8a3346f90
samirvithlani <samir.vithlani83955@gmail.com>
2024-08-09 18:33:27 +0530

    array

--- array/array3.js
+++ array/array3.js
@@ -2,22 +2,41 @@ var lang = ["hindi","english","marathi","tamil","telugu","gujarati"];
 
 //loop
 //lang[i]
-var x = lang.some((l)=>{
+// var x = lang.some((l)=>{
 
-    //return predicate.. true or false
-    //return  l.startsWith("x")
-    return l.length > 10
-})
+//     //return predicate.. true or false
+//     //return  l.startsWith("x")
+//     return l.length > 10
+// })
 
-console.log(x) //true
+// console.log(x) //true
 
-var x = lang.every((l)=>{
-    return l.length > 5
-})
+// var x = lang.every((l)=>{
+//     return l.length > 5
+// })
 
-console.log(x) //true
+// console.log(x) //true
 
 
-var x = lang.some((l)=>l.includes("a"))
-console.log(x) //true
+// var x = lang.some((l)=>l.includes("a"))
+// console.log(x) //true
+
+
+var flag = false
+
+
+for(let i=0;i<lang.length;i++){
+
+    if(lang[i].length<=5){
+        flag = false
+        break
+    }
+    else{
+        flag = true
+    }
+
+
+}
+
+console.log(flag) //true
 
