557113ca061491929ce5f4b6fc017363d078ba8a
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-13 18:23:26 +0530

    dination camp

--- forms/form1.js
+++ forms/form1.js
@@ -8,6 +8,13 @@ const submitHandler = (event) => {
     const email = document.getElementById("email").value;
     console.log("email = ",email);
 
+    const gender = document.getElementById("male").checked ? "male" :
+    document.getElementById("female").checked ? "female" : "other";
+    console.log("gender...",gender)
+
+    const country = document.getElementById("country").value;
+    console.log("country = ",country);
+
 
 
 }
