c43e884197ad95c0bde3ea6dd95f6ab5a1d7ec86
samirvithlani <samir.vithlani83955@gmail.com>
2024-12-13 17:20:19 +0530

    get api

---
+++ fetch/printData.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded",async()=>{
+
+    // const products = [
+    //     {
+    //         id:1,
+    //         title:"abc",
+    //         price:100,
+    //         category:"Beauty"
+    //     }
+    // ]
+
+    const res = await fetch("https://dummyjson.com/products",{
+        method:"GET"
+    })
+    const data = await res.json()
+    console.log(data)
+
+    data.products.forEach((product)=>{
+        createRow(product)
+    })
+
+})
+
+
+const createRow = (product)=>{
+
+
+    const tbody = document.getElementById("tbody")
+    const tr = document.createElement("tr")
+
+    const idTd = document.createElement("td")
+    idTd.innerHTML = product.id
+
+    const titleTd = document.createElement("td")
+    titleTd.innerHTML = product.title
+
+    const priceTd = document.createElement("td")
+    priceTd.innerHTML = product.price
+
+    const categoryTd = document.createElement("td")
+    categoryTd.innerHTML = product.category
+
+    const tagsTd = document.createElement("td")
+    tagsTd.innerHTML = product.tags
+
+
+
+    const imageTd  = document.createElement("td")
+    const img = document.createElement("img")
+    img.style.height="70px"
+    img.style.width="70px"
+
+    img.src = product.thumbnail
+    imageTd.appendChild(img)
+
+    tr.appendChild(idTd)
+    tr.appendChild(titleTd)
+    tr.appendChild(priceTd)
+    tr.appendChild(categoryTd)
+    tr.appendChild(tagsTd)
+    tr.appendChild(imageTd)
+
+    tbody.appendChild(tr)
+
+
+
+
+
+}
